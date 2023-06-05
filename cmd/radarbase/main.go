8c5ca915966633b63b2e93acd970e267bd4834df
Angold-4 <wangold4w@gmail.com>
2023-06-06 02:58:57 +0800

    impl radarbase app, curl test passed

---
+++ cmd/radarbase/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"radarbase/pkg/api"
+	"radarbase/pkg/excel"
+	"radarbase/pkg/mdb"
+)
+
+// Helper function to load a single sheet into the database
+func loadSheetIntoDB(db *mdb.MDB, filePath string, sheetName string) error {
+	// Parse row data
+	rowData, _, err := excel.RowParse(filePath, sheetName)
+	if err != nil {
+		return err
+	}
+
+	// Parse columnar data
+	colData, colHeaders, err := excel.ColParse(filePath, sheetName)
+	if err != nil {
+		return err
+	}
+
+	// Inserting the row data
+	err = db.RowLoadToDB(rowData, sheetName)
+	if err != nil {
+		return err
+	}
+
+	// Inserting the columnar data
+	err = db.ColLoadToDB(colData, colHeaders, sheetName)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	db, err := mdb.NewMDB("mongodb://localhost:27017", "RowDB", "ColDB")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// List all the sheet names that you need to load
+	sheets := []string{"Sec", "Ind", "StkSH", "StkSZ", "StkHK"}
+	filePath := "files/radar.xlsm"
+
+	for _, sheet := range sheets {
+		err = loadSheetIntoDB(db, filePath, sheet)
+		if err != nil {
+			fmt.Printf("Failed to load sheet %s: %v\n", sheet, err)
+			os.Exit(1)
+		}
+	}
+
+	// Initialize API
+	apiHandler := api.NewAPI(db)
+
+	// Start server
+	srv := &http.Server{
+		Handler: apiHandler.SetupRouter(), // use the SetupRouter method here
+		Addr:    "127.0.0.1:8080",
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			// It's fine to panic here, as this should never happen when closing the server
+			log.Panic(err)
+		}
+	}()
+
+	// Wait for Ctrl+C to exit
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	// Block until a signal is received
+	<-c
+
+	// Cleanup and shutdown
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// Extra handling here
+		cancel()
+	}()
+
+	if err = srv.Shutdown(ctxShutDown); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+
+	log.Println("Shutting down gracefully, bye bye ...")
+	os.Exit(0)
+}
---
+++ cmd/radarbase/test/requests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Starting tests..."
+
+# Wait for server to start
+echo "Waiting for server to start..."
+sleep 5  # Adjust this if your server takes longer to start
+
+# Test GetByHeadersHandler
+echo "Testing GetByHeadersHandler..."
+headers="基本分析分數,技術分析分數,保留盈餘增長标准分数,基因分析標準分數"
+encodedHeaders=$(echo -n $headers | jq -sRr @uri)
+response=$(curl -s "http://localhost:8080/api/v1/StkHK/item?headers=${encodedHeaders}")
+echo "Response: ${response}"
+echo
+
+# Test GetSingleRecordHandler
+echo "Testing GetSingleRecordHandler..."
+stockName="1112HK-H&H國際控股"
+encodedStockName=$(echo -n $stockName | jq -sRr @uri)
+response=$(curl -s "http://localhost:8080/api/v1/StkHK/item/${encodedStockName}")
+echo "Response: ${response}"
+echo
+
+# Test GetHeadersHandler
+echo "Testing GetHeadersHandler..."
+response=$(curl -s "http://localhost:8080/api/v1/headers/StkHK")
+echo "Response: ${response}"
+echo
+
+echo "Finished tests."
